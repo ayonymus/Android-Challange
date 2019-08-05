af9ae78b903bf2f98fc27bc72a7876eaab997438
Gabor Tatrai <gabor.tatra.dev@gmail.com>
2019-08-05 21:26:18 +0100

    Add Okhttp

---
+++ app/src/main/java/com/ayonymus/androidchallenge/di/NetworkModule.kt
@@ -0,0 +1,39 @@
+package com.ayonymus.androidchallenge.di
+
+import com.ayonymus.androidchallenge.BuildConfig
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    internal fun provideLogingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = when {
+                BuildConfig.DEBUG -> HttpLoggingInterceptor.Level.BODY
+                else -> HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Provides
+    internal fun retrofitBuilder(interceptor: HttpLoggingInterceptor): Retrofit.Builder {
+        return Retrofit.Builder()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build()
+            )
+    }
+
+}
