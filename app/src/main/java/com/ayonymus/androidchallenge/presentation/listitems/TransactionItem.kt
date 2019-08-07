b8fbad5cf4edc1a5253e818f14ce3abfce4259e7
Gabor Tatrai <gabor.tatra.dev@gmail.com>
2019-08-08 00:26:11 +0100

    Add icon for incoming and outgoing transactions

--- app/src/main/java/com/ayonymus/androidchallenge/presentation/listitems/TransactionItem.kt
+++ app/src/main/java/com/ayonymus/androidchallenge/presentation/listitems/TransactionItem.kt
@@ -1,5 +1,6 @@
 package com.ayonymus.androidchallenge.presentation.listitems
 
+import androidx.core.content.ContextCompat
 import com.ayonymus.androidchallenge.R
 import com.ayonymus.androidchallenge.domain.entity.Transaction
 import com.ayonymus.androidchallenge.presentation.satoshiToBtc
@@ -15,6 +16,11 @@ class TransactionItem(private val transaction: Transaction): Item() {
         viewHolder.itemView.apply {
             text_transaction_value.text = transaction.value.satoshiToBtc()
             text_date.text = dateFormat.format(Date(transaction.time * 1000))
+            imageView.setImageDrawable(ContextCompat.getDrawable(context, if (transaction.value >= 0) {
+                R.drawable.ic_bitcoin_in
+            } else {
+                R.drawable.ic_bitcoin_out
+            }))
         }
 
     }
@@ -22,6 +28,7 @@ class TransactionItem(private val transaction: Transaction): Item() {
     override fun getLayout() = R.layout.item_transaction
 
     companion object {
+        // This is not the best solution for internationalization. Maybe jodatime would be better.
         private val dateFormat = SimpleDateFormat("MM/dd/yyyy HH:mm", Locale.UK)
     }
 
